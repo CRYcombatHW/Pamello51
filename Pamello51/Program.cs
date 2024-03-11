0c4dab68e4287509f8c9a76890c5633c579065ee
CRYcombat <koteika5063@gmail.com>
2024-03-11 18:07:45 +0400

    Added configuration

--- Pamello51/Program.cs
+++ Pamello51/Program.cs
@@ -1,5 +1,7 @@
-﻿using Discord;
+﻿using AngleSharp;
+using Discord;
 using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -9,16 +11,23 @@ namespace Pamello51
 	{
 		public static DiscordSocketClient Client;
 
+		public static IConfigurationRoot Configuration;
+
 		public static async Task Main(string[] args) {
 			Client = new DiscordSocketClient(new DiscordSocketConfig {
 				GatewayIntents = GatewayIntents.All,
 				AlwaysDownloadUsers = true
 			});
 
+			Configuration = new ConfigurationBuilder()
+				.SetBasePath(AppContext.BaseDirectory)
+				.AddYamlFile("config.yaml")
+				.Build();
+
 			Client.Log += async (message) => Console.WriteLine($"[log] {message}");
 			Client.Ready += async () => Console.WriteLine($"{Client.CurrentUser.Username} Ready");
 
-			await Client.LoginAsync(TokenType.Bot, "");
+			await Client.LoginAsync(TokenType.Bot, Configuration["token"] ?? throw new Exception("cant find token in configuration file"));
 			await Client.StartAsync();
 
 			await Task.Delay(-1);
