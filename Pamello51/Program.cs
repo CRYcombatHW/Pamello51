da9b462225a46529f7355f790f2e8e2afd095510
CRYcombat <koteika5063@gmail.com>
2024-03-11 14:04:45 +0400

    Added main fuctional to Program class

--- Pamello51/Program.cs
+++ Pamello51/Program.cs
@@ -1,9 +1,38 @@
-﻿namespace Pamello51
+﻿using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Pamello51
 {
-	internal class Program
+	public class Program
 	{
-		static void Main(string[] args) {
-			Console.WriteLine("Hello, World!");
+		public static Task Main() => new Program().MainAsync();
+
+		public async Task MainAsync() {
+			using IHost host = Host.CreateDefaultBuilder()
+				.ConfigureServices((_, services) =>
+				services.AddSingleton(x => new DiscordSocketClient(new DiscordSocketConfig {
+					GatewayIntents = GatewayIntents.All,
+					AlwaysDownloadUsers = true
+				}))).Build();
+
+			await RunAsync(host);
+		}
+
+		public async Task RunAsync(IHost host) {
+			using IServiceScope serviceScope = host.Services.CreateScope();
+			IServiceProvider provider = serviceScope.ServiceProvider;
+
+			DiscordSocketClient client = provider.GetRequiredService<DiscordSocketClient>();
+
+			client.Log += async (message) => Console.WriteLine($"[log] {message}");
+			client.Ready += async () => Console.WriteLine($"{client.CurrentUser.Username} Ready");
+
+			await client.LoginAsync(TokenType.Bot, "MTIxNjY4MjEwMjU3NDI4NDg4MQ.GHxEGe.G4Fx0OyiFytvog1GRKU5l-2LRT7ai9blMKI6aY");
+			await client.StartAsync();
+
+			await Task.Delay(-1);
 		}
 	}
 }
