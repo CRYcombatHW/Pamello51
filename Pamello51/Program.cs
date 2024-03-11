da5cc9e1bdc5ed8de94353360443d2d40ced7a79
CRYcombat <koteika5063@gmail.com>
2024-03-11 18:34:02 +0400

    Returning back dependency injection

--- Pamello51/Program.cs
+++ Pamello51/Program.cs
@@ -14,11 +14,17 @@ namespace Pamello51
 		public static IConfigurationRoot Configuration;
 
 		public static async Task Main(string[] args) {
-			Client = new DiscordSocketClient(new DiscordSocketConfig {
-				GatewayIntents = GatewayIntents.All,
-				AlwaysDownloadUsers = true
-			});
+			using IHost host = Host.CreateDefaultBuilder()
+				.ConfigureServices((_, services) =>
+				services.AddSingleton(x => new DiscordSocketClient(new DiscordSocketConfig {
+					GatewayIntents = GatewayIntents.All,
+					AlwaysDownloadUsers = true
+				}))).Build();
 
+			using IServiceScope serviceScope = host.Services.CreateScope();
+			IServiceProvider provider = serviceScope.ServiceProvider;
+
+			Client = provider.GetRequiredService<DiscordSocketClient>();
 			Configuration = new ConfigurationBuilder()
 				.SetBasePath(AppContext.BaseDirectory)
 				.AddYamlFile("config.yaml")
